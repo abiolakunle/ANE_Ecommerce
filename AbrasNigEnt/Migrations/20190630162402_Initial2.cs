089d6100972cfa6b3d04ff4d01c73eaf38b33c82
abiolakunle <42536640+abiolakunle@users.noreply.github.com>
2019-06-30 23:37:02 +0100

    database ammended worked on displaying data to machine page

---
+++ AbrasNigEnt/Migrations/20190630162402_Initial2.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbrasNigEnt.Migrations
+{
+    public partial class Initial2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SectionGroups_Machines_MachineId",
+                table: "SectionGroups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SectionGroups_MachineId",
+                table: "SectionGroups");
+
+            migrationBuilder.DropColumn(
+                name: "MachineId",
+                table: "SectionGroups");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MachineId",
+                table: "SectionGroups",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SectionGroups_MachineId",
+                table: "SectionGroups",
+                column: "MachineId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SectionGroups_Machines_MachineId",
+                table: "SectionGroups",
+                column: "MachineId",
+                principalTable: "Machines",
+                principalColumn: "MachineId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ AbrasNigEnt/Migrations/20190630173546_Initial3.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AbrasNigEnt.Migrations
+{
+    public partial class Initial3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SectionId",
+                table: "MachineSectionGroup",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MachineSectionGroup_SectionId",
+                table: "MachineSectionGroup",
+                column: "SectionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MachineSectionGroup_Sections_SectionId",
+                table: "MachineSectionGroup",
+                column: "SectionId",
+                principalTable: "Sections",
+                principalColumn: "SectionId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MachineSectionGroup_Sections_SectionId",
+                table: "MachineSectionGroup");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MachineSectionGroup_SectionId",
+                table: "MachineSectionGroup");
+
+            migrationBuilder.DropColumn(
+                name: "SectionId",
+                table: "MachineSectionGroup");
+        }
+    }
+}
